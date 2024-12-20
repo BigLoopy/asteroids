a3f031d0ae2ced07940c1e3f99883d2af5a9c646
BigLoopy <plupiani@gmail.com>
2024-12-20 08:54:00 -0500

    Player can move

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import player
 from constants import *
 
 def main():
@@ -13,14 +14,32 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player.Player.containers = (updatable, drawable)
+    player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        update(dt, updatable)
+        draw(screen, drawable)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+def update(dt, updateables):
+    for updateable in updateables:
+        updateable.update(dt)
+    pass
+
+def draw(screen, drawables):
+    for drawable in drawables:
+        drawable.draw(screen)
+    pass
 
 
 if __name__ == "__main__":
