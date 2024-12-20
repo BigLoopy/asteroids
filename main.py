8feb457bfb2ad81f752945826996bf810275330b
BigLoopy <plupiani@gmail.com>
2024-12-20 08:18:10 -0500

    Added a clock for future frame limiting, set max frames to 60

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while (True):
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
